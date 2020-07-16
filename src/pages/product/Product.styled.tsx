16efa1a62421399c3f21e5b505dc6c2248178603
MinhPhung140296 <minhphungdn@gmail.com>
2020-07-16 14:59:28 +0700

    finish

---
+++ src/pages/product/Product.styled.tsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+const StyledProduct = styled.div`
+  margin: 70px 10px 0 10px;
+`
+export default StyledProduct
---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { baseUrl } from '../../common/constants'
+import StyledProduct from './Product.styled'
+import { Header } from '../../components/Header'
+import { Footer } from '../../components/Footer'
+
+export interface IProductDetail {
+  description: string
+  name: string
+}
+
+export interface PropductDetailProps {
+  product: IProductDetail
+}
+
+const ProductDetail: React.FC<PropductDetailProps> = (props) => {
+  return (
+    <>
+      <Header />
+      <StyledProduct>
+        <h1>{props.product?.name}</h1>
+        <div dangerouslySetInnerHTML={{ __html: props.product?.description }} />
+      </StyledProduct>
+      <Footer />
+    </>
+  )
+}
+
+export async function getStaticPaths() {
+  const res = await fetch(`${baseUrl}/product/`)
+  const productList = await res.json()
+
+  const paths = productList.data.map((product) => ({
+    params: { id: product.id },
+  }))
+
+  return { paths, fallback: true }
+}
+
+export async function getStaticProps({ params }) {
+  const { id } = params
+  const res = await fetch(`${baseUrl}/product/${id}`)
+  const product = await res.json()
+
+  return {
+    props: {
+      product,
+    },
+  }
+}
+
+export default ProductDetail
